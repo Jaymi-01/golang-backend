07e77bf47d2034009dbd44f60f575f6ada8764eb
Joel Miller <millerjoel7597@gmail.com>
2026-06-08 14:54:11 +0100

    Data types (#2)

--- main.go
+++ main.go
@@ -11,4 +11,7 @@ func main() {
 	fmt.Printf("we have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get Your Tickets Here")
 
+	var userName string //You need to declare the data type of the variable when you declare it
+	var userTickets int
+
 }
